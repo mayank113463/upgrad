940be7ce93143649a6c5062d5da820e1311e5c6d
mayank113463 <mayank113463@gmail.com>
2022-02-21 11:24:56 +0530

    Create even_odd2.py

---
+++ even_odd2.py
@@ -0,0 +1,12 @@
+#input has been taken for you
+
+k=int(input())
+
+if k%3==0: #if k is of form 3n
+    print('sexy')
+elif k%3==1: #else if k is of form 3n+1
+    print('beautiful')
+elif k%3==2: #else if k is of form 3n+2
+    print('pretty')
+#check if the number is beautiful, pretty or sexy
+
